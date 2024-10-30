dc616770544596c627b6c81f20f4ea1656c49163
rizwanc018 <rizwanc018@gmail.com>
2024-10-30 15:09:36 +0530

    transfer minted coins

--- dist/index.js
+++ dist/index.js
@@ -13,24 +13,42 @@ const web3_js_1 = require("@solana/web3.js");
 // import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token"
 const spl_token_1 = require("@solana/spl-token");
 const airdrop_1 = require("./airdrop");
-// connection: Connection,
-// payer: Signer,
-// mintAuthority: PublicKey,
-// freezeAuthority: PublicKey | null,
-// decimals: number,
-// keypair = Keypair.generate(),
-// confirmOptions?: ConfirmOptions,
-// programId = TOKEN_PROGRAM_ID,
 const startMint = (mintWallet) => __awaiter(void 0, void 0, void 0, function* () {
     const connection = new web3_js_1.Connection("http://localhost:8899", "confirmed");
     const creatorTokenAddress = yield (0, spl_token_1.createMint)(connection, mintWallet, mintWallet.publicKey, null, 9, undefined, { commitment: "confirmed" }, spl_token_1.TOKEN_PROGRAM_ID);
     console.log("Token address: ", creatorTokenAddress.toBase58());
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
     return creatorTokenAddress;
 });
+const transferTokens = (creatorTokenAddress, mintWallet, receiver) => __awaiter(void 0, void 0, void 0, function* () {
+    const connection = new web3_js_1.Connection("http://localhost:8899", "confirmed");
+    const tokenAccount = yield (0, spl_token_1.getOrCreateAssociatedTokenAccount)(connection, mintWallet, creatorTokenAddress, mintWallet.publicKey);
+    console.log("Token account: ", tokenAccount);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+    // Add supply of 999 tokens
+    const mintToken = yield (0, spl_token_1.mintTo)(connection, mintWallet, creatorTokenAddress, tokenAccount.address, mintWallet, 999 * web3_js_1.LAMPORTS_PER_SOL);
+    console.log("Mint token: ", mintToken);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+    const senderTokenAccount = yield (0, spl_token_1.getOrCreateAssociatedTokenAccount)(connection, mintWallet, creatorTokenAddress, mintWallet.publicKey);
+    console.log("Sender token account: ", senderTokenAccount);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+    const receiverTokenAccount = yield (0, spl_token_1.getOrCreateAssociatedTokenAccount)(connection, mintWallet, // Fee payer or signer
+    creatorTokenAddress, receiver);
+    console.log("Receiver token account: ", receiverTokenAccount);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+    const transaction = new web3_js_1.Transaction().add((0, spl_token_1.createTransferInstruction)(senderTokenAccount.address, receiverTokenAccount.address, mintWallet.publicKey, 100 * web3_js_1.LAMPORTS_PER_SOL));
+    console.log("Transaction: ", transaction);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+    const signature = yield (0, web3_js_1.sendAndConfirmTransaction)(connection, transaction, [mintWallet]);
+    console.log("Transaction Signature: ", signature);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+});
 (() => __awaiter(void 0, void 0, void 0, function* () {
+    // mintAuthority
     const mintWallet = yield web3_js_1.Keypair.generate();
-    console.log("🚀 ~ file: index.ts:40 ~ ; ~ mintWallet🚀", mintWallet);
-    yield (0, airdrop_1.airdrop)(mintWallet.publicKey, 1);
+    yield (0, airdrop_1.airdrop)(mintWallet.publicKey, 1000);
+    // createdTokenAddress
     const creatorTokenAddress = yield startMint(mintWallet);
+    yield transferTokens(creatorTokenAddress, mintWallet, new web3_js_1.PublicKey("7zcswFw65sppQrU9sZD5VT3g2AL4BwLbqmkbiRQTNg61"));
 }))();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,23 +1,19 @@
-import { Keypair, Connection, PublicKey } from "@solana/web3.js"
-// import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token"
 import {
-    getMint,
+    Keypair,
+    Connection,
+    PublicKey,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    sendAndConfirmTransaction,
+} from "@solana/web3.js"
+import {
     getOrCreateAssociatedTokenAccount,
     createTransferInstruction,
     TOKEN_PROGRAM_ID,
-    createMintToInstruction,
     createMint,
+    mintTo,
 } from "@solana/spl-token"
 import { airdrop } from "./airdrop"
-
-// connection: Connection,
-// payer: Signer,
-// mintAuthority: PublicKey,
-// freezeAuthority: PublicKey | null,
-// decimals: number,
-// keypair = Keypair.generate(),
-// confirmOptions?: ConfirmOptions,
-// programId = TOKEN_PROGRAM_ID,
 
 const startMint = async (mintWallet: Keypair) => {
     const connection = new Connection("http://localhost:8899", "confirmed")
@@ -31,14 +27,72 @@ const startMint = async (mintWallet: Keypair) => {
         { commitment: "confirmed" },
         TOKEN_PROGRAM_ID
     )
-    console.log("Token address: ", creatorTokenAddress.toBase58())
+
     return creatorTokenAddress
 }
 
+const transferTokens = async (creatorTokenAddress: PublicKey, mintWallet: Keypair, receiver: PublicKey) => {
+    const connection = new Connection("http://localhost:8899", "confirmed")
+
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        mintWallet,
+        creatorTokenAddress,
+        mintWallet.publicKey
+    )
+
+
+    // Add supply of 999 tokens
+    const mintToken = await mintTo(
+        connection,
+        mintWallet,
+        creatorTokenAddress,
+        tokenAccount.address,
+        mintWallet,
+        999 * LAMPORTS_PER_SOL
+    )
+
+
+    const senderTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        mintWallet,
+        creatorTokenAddress,
+        mintWallet.publicKey
+    )
+
+    const receiverTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        mintWallet, // Fee payer or signer
+        creatorTokenAddress,
+        receiver
+    )
+
+
+    const transaction = new Transaction().add(
+        createTransferInstruction(
+            senderTokenAccount.address,
+            receiverTokenAccount.address,
+            mintWallet.publicKey,
+            100 * LAMPORTS_PER_SOL
+        )
+    )
+
+    const signature = await sendAndConfirmTransaction(connection, transaction, [mintWallet])
+    console.log("Transaction Signature: ", signature)
+}
+
+//
 ;(async () => {
+    // mintAuthority
     const mintWallet = await Keypair.generate()
-    console.log("🚀 ~ file: index.ts:40 ~ ; ~ mintWallet🚀", mintWallet)
 
     await airdrop(mintWallet.publicKey, 1000)
+    // createdTokenAddress
     const creatorTokenAddress = await startMint(mintWallet)
+
+    await transferTokens(
+        creatorTokenAddress,
+        mintWallet,
+        new PublicKey("7zcswFw65sppQrU9sZD5VT3g2AL4BwLbqmkbiRQTNg61")
+    )
 })()
